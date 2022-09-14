76630778610d4e576f7f8b20e509d7dca0277120
DaniaM21 <danianunez19@gmail.com>
2022-09-13 18:45:35 -0700

    added Optional 2 (rounded corners and shadows) to Lab 1

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,10 +11,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // gives the card and labels rounded corners
+        card.layer.cornerRadius = 20.0
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        
+        // adds shadows to card
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
